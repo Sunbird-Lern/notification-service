824d255569c368f8cee0fac003f20524f02d25a5
manzarul <manzarul07@gmail.com>
2019-09-10 14:24:22 +0530

    Issue #SB-14607 fix: adding env

--- setVars.sh
+++ setVars.sh
@@ -2,5 +2,7 @@
 
 ################### NOTIFICATION_SERVICE configuration ###################################
 #export sunbird_notification_fcm_account_key=
+#export sunbird_notification_kafka_servers_config=
+#export sunbird_notification_kafka_topic=
 
 ################### END - NOTIFICATION_SERVICE configuration #############################
