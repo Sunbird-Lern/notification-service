15d3a9e6f1fe9cb05f53135135f5c7b99a212416
AMIT KUMAR <amit.kumar@tarento.com>
2020-06-29 13:38:16 +0530

    Issue #SB-19466 feat:changed akka configuration

--- notification-sdk/src/test/java/org/sunbird/notification/sms/Message91Test.java
+++ notification-sdk/src/test/java/org/sunbird/notification/sms/Message91Test.java
@@ -12,6 +12,7 @@ import org.sunbird.notification.utils.SMSFactory;
 
 public class Message91Test extends BaseMessageTest {
   SMSConfig config = new SMSConfig(null, "TESTSU");
+  ISmsProvider object = SMSFactory.getInstance("91SMS", config);
 
   @Test
   public void testInitSuccess() {
@@ -28,90 +29,78 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testGetInstanceSuccessWithName() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     Assert.assertTrue(object instanceof Msg91SmsProviderImpl);
   }
 
   @Test
   public void testSendSuccess() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
+
     boolean response = object.sendSms("9666666666", "test sms");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendFailureWithFormattedPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("(966) 3890-445", "test sms 122");
     Assert.assertFalse(response);
   }
 
   @Test
   public void testSendSuccessWithoutCountryCodeArg() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("919666666666", "test sms 122");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendSuccessWithoutCountryCodeArgAndPlus() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("+919666666666", "test sms 122");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendFailureWithEmptyPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("", "test sms 122");
     Assert.assertFalse(response);
   }
 
   @Test
   public void testSendFailureWithEmptyMessage() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("9663890445", "");
     Assert.assertFalse(response);
   }
 
   @Test
   public void testSendWithEmptyPhoneAndMessage() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("", "");
     Assert.assertFalse(response);
   }
 
   @Test
   public void testSendFailureWithInvalidPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("981se12345", "some message");
     Assert.assertFalse(response);
   }
 
   @Test
   public void testSendSuccessWithValidPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("1111111111", "some message");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendSuccessWithCountryCode() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("1234567898", "91", "some message");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendSuccessWithCountryCodeAndPlus() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     boolean response = object.sendSms("0000000000", "+91", "some message");
     Assert.assertTrue(response);
   }
 
   @Test
   public void testSendSuccessWithMultiplePhones() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     List<String> phones = new ArrayList<>();
     phones.add("1234567898");
     phones.add("1111111111");
@@ -121,7 +110,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendFailureWithMultipleInvalidPhones() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     List<String> phones = new ArrayList<>();
     phones.add("12345678");
     phones.add("11111");
@@ -131,7 +119,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendFailureWithMultipleInvalidPhonesAndEmptyMsg() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     List<String> phones = new ArrayList<>();
     phones.add("12345678");
     phones.add("11111");
@@ -141,7 +128,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendOtpSuccess() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("9663845334", "91", 5, 10, null, null);
     boolean response = object.sendOtp(request);
     Assert.assertTrue(response);
@@ -149,7 +135,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendOtpFailureWithIncorrectPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("96638453", "91", 5, 10, null, null);
     boolean response = object.sendOtp(request);
     Assert.assertFalse(response);
@@ -157,7 +142,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendOtpFailureWithPhoneLengthExceed() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("9663845354321", "91", 5, 10, null, null);
     boolean response = object.sendOtp(request);
     Assert.assertFalse(response);
@@ -165,7 +149,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendOtpFailureDueTOMinOtpLength() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("9663845354", "91", 3, 10, null, null);
     boolean response = object.sendOtp(request);
     Assert.assertFalse(response);
@@ -173,7 +156,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testSendOtpFailureDueTOMaxOtpLength() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("9663845354", "91", 10, 10, null, null);
     boolean response = object.sendOtp(request);
     Assert.assertFalse(response);
@@ -181,7 +163,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testresendOtpFailure() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("96638453", "91", 0, 10, null, null);
     boolean response = object.resendOtp(request);
     Assert.assertFalse(response);
@@ -189,7 +170,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testresendOtpFailureWithInvalidPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("96638453234", "91", 0, 10, null, null);
     boolean response = object.resendOtp(request);
     Assert.assertFalse(response);
@@ -197,7 +177,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testverifyOtpFailureWithInvalidPhone() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("96638453234", "91", 0, 10, null, null);
     boolean response = object.verifyOtp(request);
     Assert.assertFalse(response);
@@ -205,7 +184,6 @@ public class Message91Test extends BaseMessageTest {
 
   @Test
   public void testverifyOtpFailureWithInvalidOtpLength() {
-    ISmsProvider object = SMSFactory.getInstance("91SMS", config);
     OTPRequest request = new OTPRequest("96638453234", "91", 0, 10, null, "234");
     boolean response = object.verifyOtp(request);
     Assert.assertFalse(response);
