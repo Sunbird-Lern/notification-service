ac2a995a6bda767b40c2c162f5fadbe0f2922fad
sknirmalkar89 <satishkumar.nirmalkar@tarento.com>
2021-09-30 21:19:04 +0530

    SB-26937 Adding new controller test

--- service/test/controllers/notification/NotificationControllerTest.java
+++ service/test/controllers/notification/NotificationControllerTest.java
@@ -43,6 +43,28 @@ public class NotificationControllerTest extends BaseControllerTest {
   }
 
   @Test
+  public void sendSyncNotification() {
+    Map<String, Object> reqMap = new HashMap<>();
+    reqMap.put("accept", "yes");
+    Result result = performTest("/v1/notification/send/sync", "POST",reqMap);
+    assertTrue(getResponseStatus(result) == Response.Status.OK.getStatusCode());
+  }
+
+  @Test
+  public void sendV1Notification() {
+    Map<String, Object> request = getV1NotificationRequest();
+    Result result = performTest("/private/v2/notification/send", "POST",request);
+    assertTrue(getResponseStatus(result) == Response.Status.OK.getStatusCode());
+  }
+  
+  @Test
+  public void sendOTPNotification() {
+    Map<String, Object> request = getV1NotificationRequest();
+    Result result = performTest("/v1/notification/otp/verify", "POST",request);
+    assertTrue(getResponseStatus(result) == Response.Status.OK.getStatusCode());
+  }
+
+  @Test
   public void readFeedNotification() {
     Result result = performTest("/v1/notification/feed/read/12345", "GET",null);
     assertTrue(getResponseStatus(result) == Response.Status.OK.getStatusCode());
@@ -137,5 +159,19 @@ public class NotificationControllerTest extends BaseControllerTest {
     return request;
   }
 
+  private Map<String, Object> getV1NotificationRequest() {
+    Map<String,Object> request = new HashMap<>();
+    Map<String, Object> reqMap = new HashMap<>();
+    Map<String,Object> notification = new HashMap<>();
+    Map<String,Object> data = new HashMap<>();
+    data.put("actionData",new HashMap<>());
+    notification.put(JsonKey.DATA,data);
+    notification.put(JsonKey.IDS,Arrays.asList("1234"));
+    notification.put(JsonKey.TYPE,"feed");
+    notification.put("priority",1);
+    reqMap.put(JsonKey.NOTIFICATIONS,Arrays.asList(notification));
+    request.put(JsonKey.REQUEST,reqMap);
+    return request;
+  }
 
 }
