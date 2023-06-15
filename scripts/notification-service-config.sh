c94d58e84ad6795e89d3bc084d9bfd5e9a584d73
BharathwajShankar <saialumnibharathwaj@gmail.com>
2023-06-15 17:15:21 +0530

    Issue #LR-LR-449 feat: updating the readme.md file with notification service setup

---
+++ scripts/notification-service-config.sh
@@ -0,0 +1,7 @@
+export sunbird_us_system_setting_url=/api/data/v1/system/settings/list
+export sunbird_us_org_read_url=/v1/org/read
+export sunbird_msg_91_baseurl=http://api.msg91.com/
+export sunbird_msg_91_get_url=api/sendhttp.php?
+export sunbird_msg_91_post_url=api/v2/sendsms
+export sunbird_fcm_url=https://fcm.googleapis.com/fcm/send
+export sunbird_mail_server_from_email=support@open-sunbird.org
